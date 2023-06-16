cd44a5a3c1a6d46a238d9a13d95197644db5208e
Tamuzo77 <tamuzomenga@gmail.com>
2023-06-16 16:43:16 +0100

    Test de bug

--- resources/views/admin/banqueImg/index.blade.php
+++ resources/views/admin/banqueImg/index.blade.php
@@ -3,5 +3,6 @@
                     subtitle="Ici est répertorié toutes les images disponibles sur la plateforme OASIS pour toutes vos actions liées" />
 
     <x-admin.flash />
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus fugit sequi voluptate culpa, corporis expedita error fuga esse distinctio excepturi cupiditate ipsum soluta commodi ea doloribus! Odio dignissimos veritatis corporis.</p>
     <livewire:admin.image-bank />
 </x-admin.layout>
