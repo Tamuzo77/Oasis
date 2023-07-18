ea5c9ec4347e7f89e09b2659c6832ec87834e4ac
Tamuzo77 <tamuzomenga@gmail.com>
2023-07-18 17:21:38 +0100

    Paginate 5 et contenu textuel pour home

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,6 +1,6 @@
 <x-user.layout :title="$page->title" :imageAccueil="$page->imageModels[0]->image_url ?? 'img64a422e5886743.37976823/Image collée.png'">
     <x-slot name="headerText">
-            <div class="accueil-title">OASIS Consulting</div>
+            <div class="accueil-title">Bienvenue sur la plateforme de notre entreprise OASIS Consulting</div>
             <p> {{$page->header ?? ''}}</p>
     </x-slot>
     {{-- Consulting --}}
