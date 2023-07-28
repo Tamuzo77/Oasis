cc01ecae5a98258f6d41f9466916aadea20b1d88
Tamuzo77 <tamuzomenga@gmail.com>
2023-07-28 17:05:12 +0100

    Oasis modification page actus

--- resources/views/user/actu.blade.php
+++ resources/views/user/actu.blade.php
@@ -57,7 +57,7 @@
                             alt=""></a>
                     <a href="https://bj.linkedin.com/company/oasis-consulting-sarl?trk=public_jobs_topcard_logo"><img
                             src="{{ asset('oasis/accueil/Vector (3).png') }}" alt=""></a>
-                    <a href="https://www.youtube.com/@oasisconsulting3787"><img src="oasis/accueil/Vector2.png"
+                    <a href="https://www.youtube.com/@oasisconsulting3787"><img src="{{ asset('oasis/accueil/Vector2.png') }}"
                             alt=""></a>
                 </div>
                 <div class=" text-white mt-3 text-break">
