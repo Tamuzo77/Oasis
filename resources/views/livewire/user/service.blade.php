d62c0e8a6598f69cce60e69358808b32b2a85212
Tamuzo77 <tamuzomenga@gmail.com>
2023-08-09 18:10:30 +0100

    service 1/2

--- resources/views/livewire/user/service.blade.php
+++ resources/views/livewire/user/service.blade.php
@@ -2,9 +2,9 @@
     <div class="text-center mt-4">
         <h2>Consultez nos services</h2>
     </div>
-    <div class="row justify-content-center mx-5">
+    <div class="row justify-content-center mx-5 services">
         @forelse ($services as $service)
-            <a href="/services" class="col-lg-4 col-sm-6 col-12 mt-4">
+            <a href="{{ $service->name == 'Formations' ? '/formations-certifiantes_oasis' : '/services' }}" class="col-lg-4 col-sm-6 col-12 mt-4">
                 <img class="w-100" src="{{ asset("storage/$service->cover_image") }}" alt="">
             </a>
         @empty
