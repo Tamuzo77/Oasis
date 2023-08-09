eeafd1d56a6072e7acb340ba788b061fed98362f
Tamuzo77 <tamuzomenga@gmail.com>
2023-08-09 10:00:54 +0100

    Whatsapp fonctionnel

--- resources/views/components/partials/whatsapp.blade.php
+++ resources/views/components/partials/whatsapp.blade.php
@@ -1,5 +1,5 @@
 
-    <a href="" class="whatsapp">
+    <a href="https://wa.me/96856969" class="whatsapp">
         <i class="fa-brands fa-whatsapp rounded-circle px-2 py-1 me-3" style="color: #ffffff;height:65px;background-color:green;right:2%; position:fixed;z-index:5;bottom:50px;"></i>
        </a>
 
